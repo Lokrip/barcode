1a8dfb4a1e17db88addc365df57b9ef22630e8b4
Lokrip <slava.dorokhov.08@mail.ru>
2025-05-12 16:39:38 +0600

    feat: add layouts

--- packages/shared/src/shared/ui/container/container.tsx
+++ packages/shared/src/shared/ui/container/container.tsx
@@ -2,11 +2,9 @@ import { FC, memo } from "react";
 import { ContainerProps } from "./container-type";
 import { correctClass } from "../../utils/utils";
 
-import styles from "./styles/container.module.scss"
-
 export const Container: FC<ContainerProps> = memo(({maxWidth, className, children}) => {
     const computedMaxWidth = typeof maxWidth === "number" ? `${maxWidth}px` : maxWidth;
-    const styleCorrect = correctClass(styles.container, className ?? "")
+    const styleCorrect = correctClass("container", className ?? "")
 
     return (
         <div style={{maxWidth: computedMaxWidth}} className={styleCorrect}>
