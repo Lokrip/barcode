46d29a0bd2ae78e4cf24f8e4452ce4a4f289fb7b
Lokrip <slava.dorokhov.08@mail.ru>
2025-05-14 16:18:20 +0600

    feat: интегрировал в модуль shared модуль sass, создал 2 layouts для стилей

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -5,12 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [
     react()
-  ],
-  css: {
-    preprocessorOptions: {
-        sass: {
-            additionalData: "@import \"./src/sass/_index.scss\""
-        }
-    }
-  }
+  ]
 })
