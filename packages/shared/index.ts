10cd70e48b3c90201538ace191000107cb161cea
Noty___ <noty.trreeed@gmail.com>
2025-05-19 22:13:09 +0600

    feat: add Button and Dropdown exports

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -2,4 +2,10 @@ export { Header } from "./src/widgets/header";
 
 export { Layouts } from "./src/widgets/layouts";
 
-export { Skeleton } from "./src/shared/ui/skeleton"
+export { Skeleton } from "./src/shared/ui/skeleton";
+
+export { Button } from  "./src/shared/ui/button";
+
+export { Dropdown } from "./src/shared/ui/dropdown";
+
+export { DropdownOption } from "./src/shared/ui/dropdown";
