b2573703efb89931d33c9bb396641615709989f7
Noty___ <noty.trreeed@gmail.com>
2025-05-19 22:10:01 +0600

    refactor: update dropdown structure

--- packages/shared/src/shared/ui/dropdown/dropdown.tsx
+++ packages/shared/src/shared/ui/dropdown/dropdown.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { FC, ChangeEvent } from 'react';
 import { DropdownProps } from './model/dropdown-props.ts';
--- packages/shared/src/shared/ui/dropdown/index.ts
+++ packages/shared/src/shared/ui/dropdown/index.ts
@@ -1,1 +1,3 @@
 export { Dropdown } from './dropdown';
+
+export { DropdownOption } from './_ui/dropdownoption.tsx';
