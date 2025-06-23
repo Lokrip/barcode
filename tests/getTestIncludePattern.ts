7303f6f79388669c6d399b75308310e5ffeba05a
Lokrip <lokrip2@gmail.com>
2025-06-23 09:48:27 +0600

    asdasda

--- tests/getTestIncludePattern.ts
+++ tests/getTestIncludePattern.ts
@@ -3,14 +3,14 @@ import type { TestType } from "./types/test-types";
 export function getTestIncludePattern(testType: TestType = "unit"): string {
     switch (testType) {
         case "unit":
-            return "**/*.unit.test.ts";
+            return "**/*.unit.test.{ts,tsx}";
         case "integration":
-            return "**/*.integration.test.ts";
+            return "**/*.integration.test.{ts,tsx}";
         case "e2e":
-            return "**/*.e2e.test.ts";
+            return "**/*.e2e.test.{ts,tsx}";
         case "snapshot":
-            return "**/*.snapshot.test.ts";
+            return "**/*.snapshot.test.{ts,tsx}";
         default:
-            return "**/*.test.ts"; // fallback — все тесты
+            return "**/*.test.{ts,tsx}"; // fallback — все тесты
     }
 }
